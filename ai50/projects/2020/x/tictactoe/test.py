01b863a7dfc9df54edd62220ac29e31d83f7af89
Pascal Aubort <pascal.aubort@gmail.com>
2022-04-03 12:33:25 +0000

    Implement actions function with tests

--- ai50/projects/2020/x/tictactoe/test.py
+++ ai50/projects/2020/x/tictactoe/test.py
@@ -9,7 +9,7 @@ EMPTY = None
 class TestTicTacToe(unittest.TestCase):
     def test_player(self):
         """
-        Test that it can sum a list of integers
+        Test player turn
         """
         board = [[EMPTY, EMPTY, EMPTY],
                 [EMPTY, EMPTY, EMPTY],
@@ -38,6 +38,38 @@ class TestTicTacToe(unittest.TestCase):
         
         self.assertEqual(ttt.player(board), None)
 
+
+    def test_actions(self):
+        """
+        Test player turn
+        """
+        board = [[EMPTY, EMPTY, EMPTY],
+                [EMPTY, EMPTY, EMPTY],
+                [EMPTY, EMPTY, EMPTY]]
+
+        actions = {(0,0), (0,1), (0,2), 
+                    (1,0), (1,1), (1,2),
+                    (2,0), (2,1), (2,2)}
+        
+        self.assertEqual(ttt.actions(board), actions)
+
+        board = [[X, EMPTY, O],
+                [EMPTY, EMPTY, O],
+                [EMPTY, X, EMPTY]]
+
+        actions = {(0,1), 
+                    (1,0), (1,1),
+                    (2,0), (2,2)}
+        
+        self.assertEqual(ttt.actions(board), actions)
+
+        board = [[X, X, O],
+                [X, O, O],
+                [O, X, O]]
+
+        actions = set()
+        
+        self.assertEqual(ttt.actions(board), actions)
 
 if __name__ == '__main__':
     unittest.main()
